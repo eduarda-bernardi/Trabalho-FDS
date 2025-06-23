f8de17ff0575d0ba03193a3dd5663ee5546a0626
Eduarda Bernardi <duda2010bernardi@gmail.com>
2025-06-23 11:06:37 -0300

    mensagem e fontes adicionadas

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 var button = document.getElementById("button");
-            var mensagem = document.getElementById("mensagem");
+            
+var mensagem = document.getElementById("mensagem");
 
-            function mostraMensagem(){
-                mensagem.style.display = "block";
-                button.style.display = "none";
-            }
+function mostraMensagem(){
+    mensagem.style.display = "block";
+    button.style.display = "none";
+}
