7cf687e314d21a91a4c37a214a268539db4abe4a
Eduarda Bernardi <duda2010bernardi@gmail.com>
2025-06-23 01:01:02 -0300

    arquivos adicionados

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var button = document.getElementById("button");
+            var mensagem = document.getElementById("mensagem");
+
+            function mostraMensagem(){
+                mensagem.style.display = "block";
+                button.style.display = "none";
+            }
